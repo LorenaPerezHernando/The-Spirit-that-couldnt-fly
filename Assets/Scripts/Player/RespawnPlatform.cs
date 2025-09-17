d1ca0e62b6e38d0c806531b04e4bf3f491954a59
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-09-17 19:39:16 +0200

    RespawnPos

--- Assets/Scripts/Player/RespawnPlatform.cs
+++ Assets/Scripts/Player/RespawnPlatform.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class RespawnPlatform : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _respawnPos;
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.position = _respawnPos.position;
+        }
     }
 }
