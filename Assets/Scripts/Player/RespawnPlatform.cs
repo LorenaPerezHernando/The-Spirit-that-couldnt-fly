d3c8c12f43b8b86f3927ddfd478548f65df3790f
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-09-17 20:08:42 +0200

    Escena

--- Assets/Scripts/Player/RespawnPlatform.cs
+++ Assets/Scripts/Player/RespawnPlatform.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 public class RespawnPlatform : MonoBehaviour
 {
     [SerializeField] private Transform _respawnPos;
+    [SerializeField] private string _respawnName;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag(_respawnName))
         {
             collision.transform.position = _respawnPos.position;
         }
