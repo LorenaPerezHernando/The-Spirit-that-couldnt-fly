4d0959485a416560217fecee10c9a10063f16e41
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-09-18 11:06:00 +0200

    Nice Number Platform

--- Assets/Scripts/Player/RespawnPlatform.cs
+++ Assets/Scripts/Player/RespawnPlatform.cs
@@ -4,13 +4,32 @@ public class RespawnPlatform : MonoBehaviour
 {
     [SerializeField] private Transform _respawnPos;
     [SerializeField] private string _respawnName;
+    [SerializeField] private GameObject[] _books;
+    [SerializeField] private Vector2[] _initialBookPos;
 
+    private void Awake()
+    {
+        for(int i = 0;  i < _books.Length; i++)
+        {
+            _initialBookPos[i] = _books[i].transform.position;                                                                                                                  
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(_respawnName))
         {
             collision.transform.position = _respawnPos.position;
+            ResetBooks();
+        }
+    }
+
+    private void ResetBooks()
+    {
+        for(int i = 0; i < _books.Length; i++)
+        {
+            _books[i].transform.position = _initialBookPos[i];
+            _books[i].GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
         }
     }
 }
