271c3aed2df3e9f4350202cf22977993a48d6f0b
LorenaPerezHernando <lorenaperezhernando@gmail.com>
2025-09-26 16:06:50 +0200

    Singleton

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GameController : Singleton<GameController>
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
